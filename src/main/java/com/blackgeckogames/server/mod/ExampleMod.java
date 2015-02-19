1a68ee2a236a28c3c4f888a52aba39dbc2ac1074
Jacky2611 <jacky2611@gmail.com>
2015-02-19 18:29:16 +0100

    changed packages changed main class

--- src/main/java/com/blackgeckogames/server/mod/ExampleMod.java
+++ src/main/java/com/blackgeckogames/server/mod/ExampleMod.java
@@ -1,20 +1,34 @@
-package com.example.examplemod;
+package com.blackgeckogames.server.mod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "blackgeckoserver";
     public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void Preinit(FMLPreInitializationEvent event)
+    {
+
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+    
+    @EventHandler
+    public void Postinit(FMLPostInitializationEvent event)
+    {
+		
     }
 }
