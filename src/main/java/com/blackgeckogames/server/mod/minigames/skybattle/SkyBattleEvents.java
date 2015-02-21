7d7bce87ddc0232108bdac4eb59c81178e6c4029
Jacky2611 <jacky2611@gmail.com>
2015-02-21 20:36:17 +0100

    Modified SkyBattleEventHandler

--- src/main/java/com/blackgeckogames/server/mod/minigames/skybattle/SkyBattleEvents.java
+++ src/main/java/com/blackgeckogames/server/mod/minigames/skybattle/SkyBattleEvents.java
@@ -1,12 +1,19 @@
 package com.blackgeckogames.server.mod.minigames.skybattle;
 
+import net.minecraft.init.Blocks;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 
 public class SkyBattleEvents {
 
 	public static void onPlayerBreakEvent(BreakEvent event) {
 		System.out.println("You are not allowed to destroye this block!");
-		event.setCanceled(true);		
+		
+		if(event.world.getBlockState(event.pos).getBlock()!=Blocks.sandstone){
+			
+			event.setCanceled(true);	
+			
+		}
+	
 	}
 
 }
